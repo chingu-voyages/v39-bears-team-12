6ba97274fe864eef2205d11acd768259ae98afb7
fmtabbara <fmtabbara@hotmail.co.uk>
2022-04-09 16:33:09 +0100

    remove output dir

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var PORT = 4000;
-app.get('/', function (_, response) {
-    response.send('Hello from Bears 12 🐻');
-});
-app.listen(PORT, function () { return console.log("server running on port ".concat(PORT)); });
