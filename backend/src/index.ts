1255a3fb0d74787bf686bb5a3fc303ef4d3c752a
nasrullahiolamide <nasrullahiolamide3011@gmail.com>
2022-04-13 09:36:06 +0100

    Basic Project route implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,16 @@
 import express = require('express')
 import { userRouter } from './routes/user/users.router'
+import { projectRouter } from './routes/project/project.router'
+
+
 const app = express()
 const PORT = 4000
 
 app.use('/users', userRouter)
+app.use('/projects', projectRouter)
+
+
+
 app.get('/', (_, response) => {
   response.send('Hello from Bears 12 🐻')
 })
