c5b36be37a7c5206714cd24ea65a3a92d8492465
Davor Pernjek <pernjek.davor12@gmail.com>
2022-04-27 22:47:11 +0200

    remove navigation from app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRoutes } from './routes'
-import { Navigation } from './components'
 import './index.css'
 export const App = () => (
   <Router>
     <AppRoutes />
-    <Navigation />
   </Router>
 )
