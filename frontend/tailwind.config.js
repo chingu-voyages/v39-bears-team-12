96fd86f8e5d6aa8af9b991a53a37f6a8eb42722a
Davor Pernjek <pernjek.davor12@gmail.com>
2022-04-27 21:03:52 +0200

    add theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      orange: '#FF9F1C',
+      lightOrange: '#FFBF69',
+      white: '#FFFFFF',
+      lightCyan: '#CBF3F0',
+      cyan: '#2EC4B6',
+      dark: '#000',
+    },
   },
   plugins: [],
 }
