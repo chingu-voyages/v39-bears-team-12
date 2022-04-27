cf184a2bbfa0ff1c05cd35c38507a519d7f1951b
Davor Pernjek <pernjek.davor12@gmail.com>
2022-04-27 22:47:35 +0200

    adjust routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home, Organisation, Projects } from '../pages'
+import NotFound from '../pages/NotFound'
 import { urls } from '../urls'
+import AdminLayout from '../utils/AdminLayout'
+import AuthLayout from '../utils/AuthLayout'
 
 export const AppRoutes = () => (
   <Routes>
-    <Route path={'/'} element={<Home />} />
-    <Route path={urls.home || '/'} element={<Home />} />
-    <Route path={urls.organisation} element={<Organisation />} />
-    <Route path={urls.projects} element={<Projects />} />
-    <Route path="*" element={<div>404</div>} />
+    <Route element={<AdminLayout />}>
+      <Route path={urls.home || '/'} element={<Home />} />
+      <Route path={urls.organisation} element={<Organisation />} />
+      <Route path={urls.projects} element={<Projects />} />
+    </Route>
+    <Route element={<AuthLayout />}></Route>
+    <Route path="*" element={<NotFound />} />
   </Routes>
 )
