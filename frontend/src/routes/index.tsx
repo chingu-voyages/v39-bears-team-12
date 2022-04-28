1848a323d3f157fd144ee87efcd7fa2e255c9826
Davor Pernjek <pernjek.davor12@gmail.com>
2022-04-28 21:59:43 +0200

    setup layouts

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home, Organisation, Projects } from '../pages'
+import Login from '../pages/Login'
 import NotFound from '../pages/NotFound'
-import { urls } from '../urls'
+import Register from '../pages/Register'
+import { adminUrls } from '../urls'
 import AdminLayout from '../utils/AdminLayout'
 import AuthLayout from '../utils/AuthLayout'
 
 export const AppRoutes = () => (
   <Routes>
     <Route element={<AdminLayout />}>
-      <Route path={urls.home || '/'} element={<Home />} />
-      <Route path={urls.organisation} element={<Organisation />} />
-      <Route path={urls.projects} element={<Projects />} />
+      <Route path={'/'} element={<Home />} />
+      <Route path={adminUrls.home} element={<Home />} />
+      <Route path={adminUrls.organisation} element={<Organisation />} />
+      <Route path={adminUrls.projects} element={<Projects />} />
     </Route>
-    <Route element={<AuthLayout />}></Route>
+    <Route element={<AuthLayout />}>
+      <Route path={'/login'} element={<Login />} />
+      <Route path={'/register'} element={<Register />} />
+    </Route>
     <Route path="*" element={<NotFound />} />
   </Routes>
 )
