8e373cdab393784d8b3e3faa2e4bb74cc122087c
Davor Pernjek <pernjek.davor12@gmail.com>
2022-05-01 20:35:06 +0200

    fix sidebar not full height when content is over 100vh

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,27 +7,29 @@ const Sidebar = () => {
     alert('Log out')
   }
   return (
-    <div className="relative bg-white dark:bg-gray-800">
+    <div className="fixed bg-white dark:bg-gray-800">
       <div className="flex flex-col sm:flex-row sm:justify-around">
         <div className="w-72 h-screen">
           <div className="flex items-center justify-start mx-6 mt-10">
             <span className="text-gray-600 dark:text-gray-300 text-2xl font-bold">TestIt</span>
           </div>
           <nav className="mt-10 px-6 ">
-            {navItems.map(({ name, path, Icon }) => (
-              <Link
-                className="hover:text-orange flex items-center p-2 my-6 transition-colors duration-200 text-gray-600 rounded-lg capitalize"
-                to={path}
-                key={name}
-              >
-                <div className="flex items-center">
-                  <div className="mr-1">{Icon}</div>
-                  <span className="text-lg font-normal">{name}</span>
-                </div>
-              </Link>
-            ))}
+            {navItems.map(
+              ({ name, path, Icon }: { name: string; path: string; Icon: React.ReactNode }) => (
+                <Link
+                  className="hover:text-orange flex items-center p-2 my-6 transition-colors duration-200 text-gray-400 rounded-lg capitalize"
+                  to={path}
+                  key={name}
+                >
+                  <div className="flex items-center">
+                    <div className="mr-1">{Icon}</div>
+                    <span className="text-lg font-normal">{name}</span>
+                  </div>
+                </Link>
+              )
+            )}
             <Link
-              className="hover:text-orange flex items-center p-2 my-6 transition-colors duration-200 text-gray-600 rounded-lg capitalize"
+              className="hover:text-orange flex items-center p-2 my-6 transition-colors duration-200 text-gray-400 rounded-lg capitalize"
               to="#"
               onClick={logOut}
             >
