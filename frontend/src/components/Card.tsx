40b3eded28ce5888e6ffbed4658b0dd89466ae0c
Davor Pernjek <pernjek.davor12@gmail.com>
2022-05-03 21:57:02 +0200

    card title has no margin if it only has title

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card = ({
   subtitle?: string
 }) => (
   <div className="bg-white shadow mt-6 rounded-lg p-6">
-    {title && <h3 className="text-gray-600 font-semibold mb-4">{title}</h3>}
+    <h3 className={`text-gray-600 font-semibold ${subtitle || (children && 'mb-4')}`}>{title}</h3>{' '}
     {subtitle && <p className="text-gray-400 font-semibold text-sm mb-4">{subtitle}</p>}
     {children && <div className="text-gray-400 text-sm mb-4">{children}</div>}
   </div>
